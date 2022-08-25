361a9cbc93f4df35c22dd18b972063fdb80d3d06
hanswatkins <hanswatkins@gmail.com>
2022-08-25 16:08:19 -0700

    creates bills list and completes routing. displays dummy data from views.py on a card in the bills route

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,10 @@ from . import views
 app_name = 'main_app'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('bills/', views.BillsList.as_view(), name='bills_list'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('login/', views.LogIn.as_view(), name='login'),
+    path('create/', views.Create.as_view(), name='create'),
+
 ]
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,45 @@
+from pipes import Template
 from django.shortcuts import render
 from django.views import View 
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
-class Home(View):
-    def get(self, request):
-        return HttpResponse("Home")
+class Home(TemplateView):
+    template_name = "main_app/home.html"
+
+class Bills(TemplateView):
+    template_name = "main_app/bills.html"
+
+class Bills:
+    def __init__(self, name, amount, your_share):
+        self.name = name
+        self.amount = amount
+        self.your_share = your_share
+
+bills = [
+    Bills("Electricity", "$125", "$50"),
+    Bills("Rent", "$1800", "$900"),
+    Bills("Insurance", "$100", "$40"),
+]
+
+class BillsList(TemplateView):
+    template_name = "main_app/bills_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["bills"] = bills
+        return context
+
+class SignUp(TemplateView):
+    template_name = "main_app/signup.html"
+
+class LogIn(TemplateView):
+    template_name = "main_app/login.html"
+
+class Create(TemplateView):
+    template_name = "main_app/create.html"
+
+
+
+
