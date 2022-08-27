487c0a263eaa12d50bf5dd032c7c22b5f56969b1
hanswatkins <hanswatkins@gmail.com>
2022-08-26 22:41:12 -0700

    adding models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Bill(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    your_split = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
