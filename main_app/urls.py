0ab800490ef0adb0e763082ae36aaee611054ac5
hanswatkins <hanswatkins@gmail.com>
2022-08-30 07:53:00 -0700

    slight styling

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('login/', views.LogIn.as_view(), name='login'),
     path('create/', views.bill_form, name='create'),
-    path('bills/add_bill', views.add_bill, name='add_bill'), 
+    path('bills/add_bill', views.add_bill, name='add_bill'),
+    path('accounts/signup/', views.Signup.as_view(), name="signup"),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,10 @@ from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Bill
 from .forms import NewBillForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 # Create your views here.
@@ -29,6 +33,8 @@ class Bills(TemplateView):
 # class BillsList(TemplateView):
 #     template_name = "main_app/bills_list.html"
 
+
+
 class SignUp(TemplateView):
     template_name = "main_app/signup.html"
 
@@ -50,12 +56,29 @@ def add_bill(request):
         new_bill = form.save(commit=False)
         new_bill.save()
         return redirect('/bills/')
-       
+
+           
 # class BillCreate(CreateView):
 #     model = Bill
 #     fields = '__all__'
 #     success_url = '/bills/'
 
+class Signup(View):
+    # show a form to fill out
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "main_app/signup.html", context)
+    # on form ssubmit validate the form and login the user.
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/bills/")
+        else:
+            context = {"form": form}
+            return render(request, "signup.html", context)
 
 
 
